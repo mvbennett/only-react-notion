575669887fdb863ded9dde8ee0284cdbe46295b0
Mike Bennett <mvbennett9@gmail.com>
2023-01-15 01:25:06 -0500

    set up build config

--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 
+const path = require('path');
+const isDev = require('electron-is-dev');
+
 require('@electron/remote/main').initialize();
+
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -11,7 +15,11 @@ const createWindow = () => {
     }
   });
 
-  win.loadURL('http://localhost:3000')
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  )
 }
 
 app.on('ready', createWindow)
